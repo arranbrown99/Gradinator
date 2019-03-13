b11ccb3580818523cffc4d1ad244a15c9014cc1d
ArranBrown <2323915b@student.gla.ac.uk>
2019-03-13 16:31:27 +0000

    Commented out registration4

--- gradinator_project/urls.py
+++ gradinator_project/urls.py
@@ -18,14 +18,13 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
-from registration.backends.simple.views import RegistrationView
+# from registration.backends.simple.views import RegistrationView
 
 from gradinator import views
 
-
-class MyRegistrationView(RegistrationView):
-    def get_success_url(self, user):
-        return '/gradinator/'
+# class MyRegistrationView(RegistrationView):
+#     def get_success_url(self, user):
+#         return '/gradinator/'
 
 
 urlpatterns = [
@@ -34,6 +33,6 @@ urlpatterns = [
                   url(r'^gradinator/', include('gradinator.urls')),
                   url(r'^admin/', admin.site.urls),
                   url(r'^accounts/', include('registration.backends.simple.urls')),
-                  url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+                  # url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
