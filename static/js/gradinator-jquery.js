421cc780120ef9b9aad2b1403a881649bbb9c373
ArranBrown <2323915b@student.gla.ac.uk>
2019-03-21 16:48:18 +0000

    Ivaylo's css stuff implemented by me credit to him though

--- static/js/gradinator-jquery.js
+++ static/js/gradinator-jquery.js
@@ -1,11 +1,17 @@
 //hides a class until the button with the correct name is pressed used in band_calculator_slug.html
 $(document).ready(function(){
     $(".to_be_hidden").hide()
+    var hidden = true
     $(".predict_btn").click(function(){
 
-
+        if (hidden == true){
         $(".p"+this.name).show()
-
+        hidden = false
+        }
+        else{
+        $(".p"+this.name).hide()
+        hidden = true
+        }
     });
 
 
